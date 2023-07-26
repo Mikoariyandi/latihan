4db6860a5a2e9b1112bbfdef9f8a648f9de6a6f5
MIKO ARIYANDI <137027381+Mikoariyandi@users.noreply.github.com>
2023-07-26 18:35:25 +0700

    uts

---
+++ Fungcion.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$nama = "Mikoariyandi";
+//echo bulan(3);
+
+function bulan($id){
+    if ($bulan == 1){
+$nama_bulan="januari";
+}elseif ($bulan==2){
+$nama_bulan="febuari";
+}elseif ($bulan==3){
+$nama_bulan="maret";
+}elseif ($bulan==4){
+$nama_bulan="april";
+ }elseif ($bulan==5){
+$nama_bulan="may";
+}elseif ($bulan==6){
+$nama_bulan="juni";
+}elseif ($bulan==7){
+$nama_bulan="juli";
+}elseif ($bulan==8){
+$nama_bulan="agustus";
+}elseif ($bulan==9){
+$nama_bulan="september";
+}elseif ($bulan==10){
+$nama_bulan="oktober";
+}elseif ($bulan==11){
+$nama_bulan="november";
+}elseif ($bulan==12){
+$nama_bulan="desember";
+}
+
+</body>
+</html>
